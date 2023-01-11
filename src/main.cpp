dead550fe163e5852c6116c26d8b56f4253baa98
Ciarán J.M <macdonald.cjm@gmail.com>
2023-01-11 21:19:24 +0000

    added some functions to main

---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+#include <sstream>
+
+// Global definitions
+
+typedef std::string Path;
+const char one = '1';
+const char zero = '0';
+const char unknown = '?';
+const char faulty = 'x';
+
+struct Node {
+    Node(char input = faulty, char output = faulty)
+    : input_value(input), output_value(output) {};
+    char input_value;
+    char output_value;
+};
+
+class Traits {
+    public : 
+        Traits(int source, int m, int n, bool debug = false)
+            : mSource(source), mM(m), mN(n), mDebug(debug) {}
+
+        Node GetSourceValue() {
+            return Node(zero, unknown);
+        };
+
+        char GetValue(char value, int source, int destination, const Path &path) {
+            if (source == mSource)
+                return (destination & 1) ? zero : one;
+            else if (source == 2)
+                return one;
+            else
+                return value;
+        }
+
+        
+}
