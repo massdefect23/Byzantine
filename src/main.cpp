9f46e24554ae6c08e2686a924e13cb6fb8ab8823
Ciarán J.M <macdonald.cjm@gmail.com>
2023-01-11 22:25:23 +0000

    added decide function

--- src/main.cpp
+++ src/main.cpp
@@ -54,3 +54,66 @@ class Traits {
         const size_t mN;
         const bool mDebug;
 };
+
+class Process {
+    public:
+        Process(int id) : mId(id) {
+            if (mChildren.size() == 0)
+                GenerateChildren(mTraits.mM, mTraits.mN, std::vector<bool>(mTraits.mN, true));
+            if (mId == mTraits.mSource)
+                mNodes[""] = mtraits.GetSourceValue();
+        }
+
+        void SendMessages(int round, std::vector<Process> &processes) {
+            for (size_t i = 0; i < mPathsByRank[round][mId].size(); i++)
+            {
+                Path source_node_path = mPathsByRank[round][mId][i];
+                source_node_path = source_node_path.substr(0, source_node_path.size() - 1);
+                Node source_node = mNodes[source_node_path];
+                for (size_t j = 0; j < mTraits.mN; j++)
+                    if (j != mTraits.mSource) {
+                        char value = mTraits.GetValue(source_node.input_value, 
+                                                        mId, 
+                                                        (int) j, 
+                                                        mPathsByRank[round][mId][i]);
+                        if (mTraits.mDebug)
+                            std::cout << "Sending from process" << mId
+                                      << " to " << static_cast<unsigned int>(j)
+                                      << ": {" << value << ", "
+                                      << mPathsByRank[round][mId][i]
+                                      << ", " << unknown << "}"
+                                      << ", getting value from source_node " << source_node_path
+                                      << "\n";
+                        processes[j].RecieveMessage(mPathsByRank[round][mId][i], Node(value, unknown));
+                    }
+            }
+        }
+
+        char Decide()
+        {
+            if (mId == mTraits.mSource)
+                return mNodes[""].input_value;
+
+            for (size_t = 0; i < mTraits.mN; i++)
+                for (size_t j = 0; j < mPathsByRank[mTraits.mM][i].size(); j++)
+                {
+                    const Path &path = mPathsByRank[mTraits.mM][i][j];
+                    Node &node = mNodes[path];
+                    node.output_value = node.input_value;
+                }
+
+            for (int round = (int) mtraits.mM - 1; round >= 0; round--)
+            {
+                for (size_t i = 0; i < mTraits.mN; i++)
+                    for (size_t j = 0; j < mPathsByRank[round][i].size(); j++)
+                    {
+                        const Path &path = mPathsByRank[round][i][j];
+                        Node &node = mNodes[path];
+                        node.output_value = GetMajority(path);
+                    }
+            }
+            const Path &top_path = mPathsByRank[0][mTraits.mSource].front();
+            const Node &top_node = mNodes[top_path];
+            return top_node.output_value;
+        }
+};
